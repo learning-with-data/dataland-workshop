487c1539817a0b56ba868b4a2c0c0cbfe7cadc35
Sayamindu Dasgupta <sayamindu@gmail.com>
2022-02-13 23:49:32 -0800

    Initial round of formatting fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Typography from "typography";
+import altonTheme from "typography-theme-alton";
+import injectFonts from "typography-inject-fonts";
+
 import { initBlockly } from "dataland-gui";
 
 import App from "./App";
@@ -8,4 +12,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 
 initBlockly();
+const typography = new Typography(altonTheme);
+typography.injectStyles();
+injectFonts(typography);
 ReactDOM.render(<App />, document.getElementById("root"));
